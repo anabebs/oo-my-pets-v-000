d5833c6014778bee52d574f00acd4daa27080f0d
Ana Bebic <anabebic.x@gmail.com>
2018-07-17 14:54:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
-class Dog
-  # code goes here
+class Dod
+ attr_reader :name 
+ attr_accessor :mood
+ def initialize(name, mood = "nervous")
+   @name = name
+   @mood = mood
+ end
+ 
+
+
+ 
+
+ 
+ 
 end
