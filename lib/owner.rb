efdeae4e355e326bdd1568a3ca441a9de3fb5f93
Ana Bebic <anabebic.x@gmail.com>
2018-07-17 14:57:12 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,6 @@
 class Owner
- attr_reader :pets, :name 
- attr_accessor :mood
+ attr_accessor :pets, :name 
+ attr_reader :mood
  def initialize(name, mood = "nervous")
    @name = name
    @mood = mood
